2678d3c9fe6f8aaf9d1f2ae4541e68195ef927a1
MatRouillard <mathis.rouillard@ensg.eu>
2024-07-10 15:21:47 +0900

    Add files to init and use GeoServer

---
+++ GeoServer/init.sh
@@ -0,0 +1,6 @@
+curl -v -u admin:geoserver -XPOST -d "<workspace><name>test</name></workspace>" -H "Content-type: text/xml"  http://localhost:8080/geoserver/rest/workspaces
+curl -v -u admin:geoserver -XPOST -T connect_omf.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores
+curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<featureType><name>edge_with_cost_tokyo_omf</name></featureType>" http://localhost:8080/geoserver/rest/workspaces/test/datastores/pgrouting_omf/featuretypes
+curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<featureType><name>node_tokyo</name></featureType>" http://localhost:8080/geoserver/rest/workspaces/test/datastores/pgrouting_omf/featuretypes
+curl -v -u admin:geoserver -XPOST -H  "accept: application/json" -H  "content-type: application/xml" -d "<rules><rule resource=\"pgrouting_omf.edge_with_cost_tokyo .r\">*</rule></rules>" http://localhost:8080/geoserver/rest/security/acl/layers
+curl -v -u admin:geoserver -XPOST -H  "accept: application/json" -H  "content-type: application/xml" -d "<rules><rule resource=\"pgrouting_omf.node_tokyo .r\">*</rule></rules>" http://localhost:8080/geoserver/rest/security/acl/layers
